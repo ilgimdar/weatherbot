0dc274f16b1a7a117c07cbf5d4f40ba7906226fa
ilgimdar <igimadeev2016@litsey2.ru>
2022-01-06 15:50:02 +0300

    cosmetic-1.0.13

--- wheather_bot.py
+++ wheather_bot.py
@@ -130,9 +130,13 @@ def location(message):
 def get_html(url, plus):
     s = requests.Session()
     print(url, plus, url + plus)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit537.36(KHTML, like Gecko) Chrome",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q = 0.9, image / webp, * / *;q = 0.8"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Language': 'en-US,en;q=0.5',
+               'Accept-Encoding': 'gzip, deflate',
+               'DNT': '1',
+               'Connection': 'keep-alive',
+               'Upgrade-Insecure-Requests': '1'}
     req = s.get(url + plus, headers=headers)
     soup = BeautifulSoup(req.text, 'html.parser')
     return soup
