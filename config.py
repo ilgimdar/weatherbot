001f0d1891384abedde092e8e44940f5403ae75c
ilgimdar <igimadeev2016@litsey2.ru>
2022-01-14 15:50:27 +0300

    cosmetic-1.8.1

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "5038004437:AAFpbEtei_2s8t4CuCVkSx696rnsyCgL6tc"
+TOKEN = "BOT-TOKEN"
 url = 'https://google-search3.p.rapidapi.com/api/v1/search/q='
 google_url = 'https://www.foreca.ru/Russia/'
